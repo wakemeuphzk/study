e6691cd61a45337c63a34568fbe865fe98a92ac4
wakemeuphzk <42692217+wakemeuphzk@users.noreply.github.com>
2018-09-15 16:47:54 +0800

    Create createFirstPlot.py

---
+++ Machine Learning/Machine Learning in Action/kNN/test/createFirstPlot.py
@@ -0,0 +1,15 @@
+import numpy as np
+import sys
+sys.path.append("kNN")
+import kNN
+import matplotlib
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_subplot(111)
+datingDataMat,datingLabels = kNN.file2matrix('kNN/data/datingTestSet.txt')
+#ax.scatter(datingDataMat[:,1], datingDataMat[:,2])
+ax.scatter(datingDataMat[:,1], datingDataMat[:,2], 15.0*np.array(datingLabels), 15.0*np.array(datingLabels))
+ax.axis([-2,25,-0.2,2.0])
+plt.xlabel('Percentage of Time Spent Playing Video Games')
+plt.ylabel('Liters of Ice Cream Consumed Per Week')
+plt.show()
